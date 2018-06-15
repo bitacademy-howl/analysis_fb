907c533c6d5d5315892574c5e19276a8a685ea90
bitacademy-howl <howl1118@gmail.com>
2018-06-15 17:19:33 +0900

    Initial commit

---
+++ __main__.py
@@ -0,0 +1,29 @@
+import collect
+import analyze
+import visualize
+
+if __name__ == '__main__':
+    items = [
+        {'pagename': 'jtbcnews', 'since': '2017-01-01', 'until': '2017-12-31'},
+        {'pagename': 'chosun', 'since': '2017-01-01', 'until': '2017-12-31'}
+    ]
+
+    # 데이터 수집(collection)
+    for item in items:
+        resultfile = collect.crawling(**item, fetch=False)
+        item['resultfile'] = resultfile
+
+    # 데이터 분석(analyze)
+    for item in items:
+        data = analyze.json_to_str(item['resultfile'], 'message')
+        item['count_wordfreq'] = analyze.count_wordfreq(data)
+
+    # 데이터 시각화(visualize)
+    for item in items:
+        count = item['count_wordfreq']
+        count_m50 = dict(count.most_common(50))
+
+        filename = "%s_%s_%s" % (item['pagename'], item['since'], item['until'])
+        visualize.wordcloud(filename, count_m50)
+        # visualize.graph_bar()
+
---
+++ __test__/test_http.py
@@ -0,0 +1,21 @@
+# http test
+
+import sys
+from urllib.request import Request, urlopen
+from datetime import *
+
+try:
+    url = 'http://www.naver.com'
+    request = Request(url)
+    resp = urlopen(request)
+    resp_body = resp.read().decode("utf-8")
+    print(resp_body)
+except Exception as e:
+    print('%s %s' % (e, datetime.now()), file=sys.stderr)
+
+"""
+System.out.println('HelloWorld');
+write(0, "Hello World")
+"""
+
+
